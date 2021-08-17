9e007d8dc8d770c9b9311a9622e08d59423970f0
harada3104 <77245451+harada3104@users.noreply.github.com>
2021-08-17 23:55:44 +0900

    ファイルのデータを読み書き　課題提出

---
+++ 2-16/index2.php
@@ -0,0 +1,30 @@
+<?php
+  echo "IT用語";
+  echo "<br><br><br>";
+
+
+  echo "CakePHPの2系・3系の違い<br>";
+  echo "パフォーマンスの向上<br>
+  ブートストラップ、ルーティング、ヘルパーテンプレートの作成などにおいて改善が行われ、パフォーマンスが向上しています。<br><br>
+  
+  
+  ビューセルの追加<br>
+  ミニコントローラーとも言えるビューセルが追加されています。
+  ナビゲーションバーのような小さな部品に適用することが可能です。<br><br>
+  
+
+  TestShellの廃止<br>
+  TestShellは廃止され、全てphpunitを介してテストを行うよう変更されています。";
+  echo "<br><br><br>";
+
+
+  echo "LAMP<br>";
+  echo "・Linux・Apache・MySQL・PHPで構成された環境が「LAMP（ランプ）」です。";
+  echo "<br><br><br>";
+
+
+
+  echo "AWS<br>";
+  echo "AWSとはAmazon Web Servicesの略で、Amazonが提供している100以上のクラウドコンピューティングサービスの総称。";
+  echo "<br><br><br><br>";
+?>
