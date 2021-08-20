405b40a3119292df5bd0be7396eaa8445b1e71e5
harada3104 <77245451+harada3104@users.noreply.github.com>
2021-08-20 16:36:37 +0900

    確認お願いします。

---
+++ 2-18/question.php
@@ -0,0 +1,52 @@
+<?php
+//POST送信で送られてきた名前を受け取って変数を作成
+$text = $_POST["text"];
+//①画像を参考に問題文の選択肢の配列を作成してください。
+$port = ["80","22","20","21"];
+$web = ["PHP","Python","JAVA","HTML"];
+$mysql =["join","select","insert","update"];
+//② ①で作成した、配列から正解の選択肢の変数を作成してください
+$seikai = $port[0];
+$seikai2 = $web[3];
+$seikai3 = $mysql[1];
+?>
+
+<link rel="stylesheet" href="style.css">
+<link rel="stylesheet" href="reset.css">
+
+
+<div id="space">
+  <p class="p">お疲れ様です<?php echo $text;?>
+  <!--POST通信で送られてきた名前を出力-->さん</p>
+  <!--フォームの作成 通信はPOST通信で-->
+  <form action="answer.php" method="post">
+      <h2>①ネットワークのポート番号は何番？</h2>
+      <!--③ 問題のradioボタンを「foreach」を使って作成する-->
+    <?php
+      $port = ["80","22","20","21"];
+      foreach ($port as $p)
+      {
+        echo '<label><input type="radio">'.$p.'</label>';
+      }
+    ?>
+      <h2>②Webページを作成するための言語は？</h2>
+      <!--③ 問題のradioボタンを「foreach」を使って作成する-->
+    <?php
+      $web = ["PHP","Python","JAVA","HTML"];
+      foreach ($web as $w)
+      {
+        echo '<label><input type="radio">'.$w.'</label>';
+      }
+    ?>  
+      <h2>③MySQLで情報を取得するためのコマンドは？</h2>
+      <!--③ 問題のradioボタンを「foreach」を使って作成する-->
+    <?php
+      $mysql =["join","select","insert","update"];
+      foreach ($mysql as $m)
+      {
+        echo '<label><input type="radio">'.$m.'</label>';
+      }
+    ?>  
+      <!--問題の正解の変数と名前の変数を[answer.php]に送る-->
+  </form>
+</div>
