afe8b88e3bde1b0162b601e9904e60e0f5592af7
harada3104 <77245451+harada3104@users.noreply.github.com>
2021-08-24 16:48:43 +0900

    アドバイスを頂きたいです。

--- 2-18/question.php
+++ 2-18/question.php
@@ -16,37 +16,43 @@ $seikai3 = $mysql[1];
 
 
 <div id="space">
-  <p class="p">お疲れ様です<?php echo $text;?>
-  <!--POST通信で送られてきた名前を出力-->さん</p>
+    <!--POST通信で送られてきた名前を出力-->
+  <p class="p">お疲れ様です<?php echo $text;?>さん</p>
   <!--フォームの作成 通信はPOST通信で-->
   <form action="answer.php" method="post">
-      <h2>①ネットワークのポート番号は何番？</h2>
-      <!--③ 問題のradioボタンを「foreach」を使って作成する-->
+    <h2>①ネットワークのポート番号は何番？</h2>
+    <!--③ 問題のradioボタンを「foreach」を使って作成する-->
     <?php
-      $port = ["80","22","20","21"];
       foreach ($port as $p)
-      {
-        echo '<label><input type="radio">'.$p.'</label>';
+      { ?>
+        <label><input type="radio" value="$p"><?php echo $p .'</label>';
       }
     ?>
-      <h2>②Webページを作成するための言語は？</h2>
-      <!--③ 問題のradioボタンを「foreach」を使って作成する-->
+
+
+    <h2>②Webページを作成するための言語は？</h2>
+    <!--③ 問題のradioボタンを「foreach」を使って作成する-->
     <?php
-      $web = ["PHP","Python","JAVA","HTML"];
       foreach ($web as $w)
-      {
-        echo '<label><input type="radio">'.$w.'</label>';
+      { ?>
+        <label><input type="radio"><?php echo $w .'</label>';
       }
-    ?>  
-      <h2>③MySQLで情報を取得するためのコマンドは？</h2>
-      <!--③ 問題のradioボタンを「foreach」を使って作成する-->
+      ?>
+
+    <h2>③MySQLで情報を取得するためのコマンドは？</h2>
+    <!--③ 問題のradioボタンを「foreach」を使って作成する-->
+    
     <?php
-      $mysql =["join","select","insert","update"];
-      foreach ($mysql as $m)
-      {
-        echo '<label><input type="radio">'.$m.'</label>';
-      }
-    ?>  
-      <!--問題の正解の変数と名前の変数を[answer.php]に送る-->
+        foreach ($mysql as $m)
+        { ?>
+          <label><input type="radio"><?php echo $m.
+          '</label>';
+        }
+      ?> 
+      <?php echo "<br>";?>
+
+    <!--問題の正解の変数と名前の変数を[answer.php]に送る-->
+    <input type="hidden" name="text" value="<?php echo $text;?>">
+    <input type="submit" value="回答する" >
   </form>
 </div>
